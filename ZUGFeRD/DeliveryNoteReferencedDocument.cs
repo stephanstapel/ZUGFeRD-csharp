6f15eed627735cb085c01f3f5e0c91b30e0e7929
Stephan Stapel <stephan.stapel@web.de>
2016-05-30 22:44:37 +0200

    added missing file

---
+++ ZUGFeRD/DeliveryNoteReferencedDocument.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace s2industries.ZUGFeRD
+{
+    public class DeliveryNoteReferencedDocument
+    {
+        public string ID { get; set; }
+        public DateTime? IssueDateTime { get; set; }
+    }
+}
