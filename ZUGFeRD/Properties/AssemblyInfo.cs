4d6747c7812d77c661ce414109bdc6c3d5f6d8de
Stephan Stapel <stephan.stapel@web.de>
2018-02-15 22:19:20 +0100

    no functional change.

--- ZUGFeRD/Properties/AssemblyInfo.cs
+++ ZUGFeRD/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("s2 industries")]
 [assembly: AssemblyProduct("ZUGFeRD")]
-[assembly: AssemblyCopyright("Stephan Stapel, s2 industries, 2016")]
+[assembly: AssemblyCopyright("Stephan Stapel, s2 industries, 2018")]
 [assembly: AssemblyTrademark("http://www.s2-industries.com")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.4.1.0")]
-[assembly: AssemblyFileVersion("2.4.1.0")]
+[assembly: AssemblyVersion("2.5.1.0")]
+[assembly: AssemblyFileVersion("2.5.1.0")]
