633f4bfa11610043eb28d0eff2aeae5badd49997
Tomcat82 <bjoern.bender@allgeier-experts.com>
2020-10-26 11:07:07 +0100

    remove duplicate/obsolete Methods for compatibility with VB.NET

--- ZUGFeRD/InvoiceValidator.cs
+++ ZUGFeRD/InvoiceValidator.cs
@@ -34,13 +34,7 @@ namespace s2industries.ZUGFeRD
     public class InvoiceValidator
     {
 
-#pragma warning disable IDE1006
-        [Obsolete("This function is deprecated. Please use ValidateAndPrint() instead.")]
-        public static void validateAndPrint(InvoiceDescriptor descriptor)
-        {
-            ValidateAndPrint(descriptor, "e:\\temp.txt");
-        }
-#pragma warning restore IDE1006
+
 
         public static void ValidateAndPrint(InvoiceDescriptor descriptor, string filename)
         {
@@ -54,14 +48,6 @@ namespace s2industries.ZUGFeRD
             }
         } // !validateAndPrint()
 
-
-#pragma warning disable IDE1006
-        [Obsolete("This function is deprecated. Please use Validate() instead.")]
-        public static List<string> validate(InvoiceDescriptor descriptor)
-        {
-            return Validate(descriptor);
-        }
-#pragma warning restore IDE1006
 
         public static List<string> Validate(InvoiceDescriptor descriptor)
         {
