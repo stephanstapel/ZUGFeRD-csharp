30cf521da5f0658f7c56bc4e28505f4ae4d79769
Stephan Stapel <stephan.stapel@web.de>
2020-11-10 20:47:25 +0100

    Protecting lineId against later change

--- ZUGFeRD/AssociatedDocument.cs
+++ ZUGFeRD/AssociatedDocument.cs
@@ -37,14 +37,14 @@ namespace s2industries.ZUGFeRD
         /// <summary>
         /// identifier of the invoice line item
         /// </summary>
-        public string LineID { get; set; }
+        public string LineID { get; internal set; }
 
 
         /// <summary>
         /// Initializes a new associated document object, optionally passing a certain lineID
         /// </summary>
         /// <param name="lineID"></param>
-        public AssociatedDocument(string lineID)
+        internal AssociatedDocument(string lineID)
         {
             this.LineID = lineID;
         }
