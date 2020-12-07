76d1f6e45e08eb1401e693959328722a10da245e
Stefan Wagner <alpini1980@gmail.com>
2020-12-07 12:19:27 +0100

    Changing List<ReceivableSpecifiedTradeAccountingAccount>() setter to internal

--- ZUGFeRD/InvoiceDescriptor.cs
+++ ZUGFeRD/InvoiceDescriptor.cs
@@ -134,7 +134,7 @@ namespace s2industries.ZUGFeRD
         public List<TradeAllowanceCharge> TradeAllowanceCharges { get; set; } = new List<TradeAllowanceCharge>();
         public PaymentTerms PaymentTerms { get; set; }        
         public InvoiceReferencedDocument InvoiceReferencedDocument { get; set; }
-        public List<ReceivableSpecifiedTradeAccountingAccount> ReceivableSpecifiedTradeAccountingAccounts { get; set; } = new List<ReceivableSpecifiedTradeAccountingAccount>();
+        public List<ReceivableSpecifiedTradeAccountingAccount> ReceivableSpecifiedTradeAccountingAccounts { get; internal set; } = new List<ReceivableSpecifiedTradeAccountingAccount>();
         public List<BankAccount> CreditorBankAccounts { get; set; } = new List<BankAccount>();
         public List<BankAccount> DebitorBankAccounts { get; set; } = new List<BankAccount>();
         public PaymentMeans PaymentMeans { get; set; }
