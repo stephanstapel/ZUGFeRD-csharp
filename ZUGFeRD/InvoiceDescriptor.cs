402eb402ec835cc9b402c1db1814ceae95061a9a
Stephan Stapel <stephan.stapel@web.de>
2022-06-29 19:28:33 +0200

    Removed obsolete function

--- ZUGFeRD/InvoiceDescriptor.cs
+++ ZUGFeRD/InvoiceDescriptor.cs
@@ -556,35 +556,8 @@ namespace s2industries.ZUGFeRD
         /// Add an additional reference document
         /// </summary>
         /// <param name="id">Document number such as delivery note no or credit memo no</param>
-        /// <param name="issueDateTime">Document Date</param>
         /// <param name="typeCode"></param>
-        /// <param name="name"></param>
-        /// <param name="referenceTypeCode">Type of the referenced document</param>
-        /// <param name="attachmentBinaryObject"></param>
-        /// <param name="filename"></param>
-        [Obsolete]
-        public void AddAdditionalReferencedDocument(string id, DateTime? issueDateTime = null, AdditionalReferencedDocumentTypeCode typeCode = AdditionalReferencedDocumentTypeCode.Unknown, string name = null, ReferenceTypeCodes referenceTypeCode = ReferenceTypeCodes.Unknown, byte[] attachmentBinaryObject = null, string filename = null)
-        {
-            this.AdditionalReferencedDocuments.Add(new AdditionalReferencedDocument()
-            {
-                ReferenceTypeCode = referenceTypeCode,
-                ID = id,
-                IssueDateTime = issueDateTime,
-                Name = name,
-                AttachmentBinaryObject = attachmentBinaryObject,
-                Filename = filename,
-                TypeCode = typeCode
-            });
-        } // !AddAdditionalReferencedDocument()
-
-
-
-        /// <summary>
-        /// Add an additional reference document
-        /// </summary>
-        /// <param name="id">Document number such as delivery note no or credit memo no</param>
-        /// <param name="issueDateTime">Document Date</param>
-        /// <param name="typeCode"></param>
+        /// <param name="issueDateTime">Document Date</param>        
         /// <param name="name"></param>
         /// <param name="referenceTypeCode">Type of the referenced document</param>
         /// <param name="attachmentBinaryObject"></param>
@@ -924,7 +897,9 @@ namespace s2industries.ZUGFeRD
                              comment: comment,
                              id: id,
                              sellerAssignedID: sellerAssignedID,
+                             buyerAssignedID: buyerAssignedID,
                              deliveryNoteID: deliveryNoteID,
+                             deliveryNoteDate: deliveryNoteDate,
                              buyerOrderID: buyerOrderID,
                              buyerOrderDate: buyerOrderDate,
                              billingPeriodStart: billingPeriodStart,
