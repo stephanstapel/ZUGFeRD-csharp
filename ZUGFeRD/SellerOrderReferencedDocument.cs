ac00ece255364ada4347c763de54b295bf0309c4
Heinz Zimmermann <Heinz.Zimmermann@accantum.de>
2022-12-19 17:29:12 +0100

    Remove wrong changes of another branch

--- ZUGFeRD/SellerOrderReferencedDocument.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace s2industries.ZUGFeRD
-{
-    /// <summary>
-    /// Structure containing details of the associated order confirmation (BT-14)
-    /// </summary>
-    public class SellerOrderReferencedDocument : BaseReferencedDocument
-    {
-    }
-}
