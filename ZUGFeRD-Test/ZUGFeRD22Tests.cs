a2e9b076f8dbcc4081457293c1eade2f76809f2d
Argollinger <43641498+Argollinger@users.noreply.github.com>
2024-07-10 19:32:33 +0200

    22Reader and Tests
    Added functionality in Reader and Test for SpecifiedTradeAllowanceCharge

--- ZUGFeRD-Test/ZUGFeRD22Tests.cs
+++ ZUGFeRD-Test/ZUGFeRD22Tests.cs
@@ -1926,5 +1926,29 @@ namespace ZUGFeRD_Test
             Assert.AreEqual(despatchAdviceNo, loadedInvoice.DespatchAdviceReferencedDocument.ID);
             Assert.AreEqual(despatchAdviceDate, loadedInvoice.DespatchAdviceReferencedDocument.IssueDateTime);
         } //!TestWriteAndReadDespatchAdviceDocumentReference
+
+        [TestMethod]
+        public void TestSpecifiedTradeAllowanceCharge()
+        {
+            InvoiceDescriptor invoice = InvoiceProvider.CreateInvoice();
+
+            invoice.TradeLineItems[0].AddSpecifiedTradeAllowanceCharge(true, CurrencyCodes.EUR, 198m, 19.8m, 10m, "Discount 10%");
+          
+            MemoryStream ms = new MemoryStream();
+            invoice.Save(ms, ZUGFeRDVersion.Version22, Profile.Extended);
+            ms.Position = 0;
+
+            InvoiceDescriptor loadedInvoice = InvoiceDescriptor.Load(ms);
+            TradeAllowanceCharge allowanceCharge = loadedInvoice.TradeLineItems[0].GetSpecifiedTradeAllowanceCharges().First();
+
+            Assert.AreEqual(allowanceCharge.ChargeIndicator, false);//false = discount
+            //CurrencyCodes are not written bei InvoiceDescriptor22Writer
+            //Assert.AreEqual(allowanceCharge.Currency, CurrencyCodes.EUR);
+            Assert.AreEqual(allowanceCharge.BasisAmount, 198m);
+            Assert.AreEqual(allowanceCharge.ActualAmount, 19.8m);
+            Assert.AreEqual(allowanceCharge.ChargePercentage, 10m);
+            Assert.AreEqual(allowanceCharge.Reason, "Discount 10%");
+        } // !SpecifiedTradeAllowanceCharge()
+
     }
 }
