ca660c8b6268256f76491bb30cf42844082222a7
Argollinger <43641498+Argollinger@users.noreply.github.com>
2024-07-10 09:54:39 +0200

    SpecifiedTradeAllowanceCharge
    Adding Functionality on InvoiceDescriptor22Writer for SpecifiedTradeAllowanceCharge.

--- ZUGFeRD/TradeLineItem.cs
+++ ZUGFeRD/TradeLineItem.cs
@@ -156,6 +156,13 @@ namespace s2industries.ZUGFeRD
         /// </summary>
         private List<TradeAllowanceCharge> _TradeAllowanceCharges { get; set; } = new List<TradeAllowanceCharge>();
 
+        /// <summary>       
+        /// A group of business terms providing information about the applicable surcharges or discounts on the total amount of the invoice item
+        /// 
+        /// Now private. Please use GetSpecifiedTradeAllowanceCharges() instead
+        /// </summary>
+        private List<TradeAllowanceCharge> SpecifiedTradeAllowanceCharges { get; set; } = new List<TradeAllowanceCharge>();
+
         /// <summary>
         /// Detailed information on the accounting reference
         /// </summary>
@@ -219,6 +226,58 @@ namespace s2industries.ZUGFeRD
         {
             return this._TradeAllowanceCharges;
         } // !GetTradeAllowanceCharges()
+
+        /// <summary>
+        /// As an allowance or charge on total item price, attaching it to the corresponding item.
+        /// </summary>
+        /// <param name="isDiscount">Marks if its an allowance (true) or charge (false). Please note that the xml will present inversed values</param>
+        /// <param name="currency">Currency of the allowance or surcharge</param>
+        /// <param name="basisAmount">Basis aount for the allowance or surcharge, typicalls the net amount of the item</param>
+        /// <param name="actualAmount">The actual allowance or surcharge amount</param>
+        /// <param name="reason">Reason for the allowance or surcharge</param>
+        public void AddSpecifiedTradeAllowanceCharge(bool isDiscount, CurrencyCodes currency, decimal? basisAmount, decimal actualAmount, string reason)
+        {
+            this.SpecifiedTradeAllowanceCharges.Add(new TradeAllowanceCharge()
+            {
+                ChargeIndicator = !isDiscount,
+                Currency = currency,
+                ActualAmount = actualAmount,
+                BasisAmount = basisAmount,
+                Reason = reason
+            });
+        } // !AddSpecifiedTradeAllowanceCharge()
+
+
+        /// <summary>
+        /// As an allowance or charge on total item price, attaching it to the corresponding item.
+        /// </summary>
+        /// <param name="isDiscount">Marks if its an allowance (true) or charge (false). Please note that the xml will present inversed values</param>
+        /// <param name="currency">Currency of the allowance or surcharge</param>
+        /// <param name="basisAmount">Basis aount for the allowance or surcharge, typicalls the net amount of the item</param>
+        /// <param name="actualAmount">The actual allowance or surcharge amount</param>
+        /// <param name="chargePercentage">Actual allowance or surcharge charge percentage</param>
+        /// <param name="reason">Reason for the allowance or surcharge</param>
+        public void AddSpecifiedTradeAllowanceCharge(bool isDiscount, CurrencyCodes currency, decimal? basisAmount, decimal actualAmount, decimal? chargePercentage, string reason)
+        {
+            this.SpecifiedTradeAllowanceCharges.Add(new TradeAllowanceCharge()
+            {
+                ChargeIndicator = !isDiscount,
+                Currency = currency,
+                ActualAmount = actualAmount,
+                BasisAmount = basisAmount,
+                ChargePercentage = chargePercentage,
+                Reason = reason
+            });
+        } // !AddSpecifiedTradeAllowanceCharge()
+
+        /// <summary>
+        /// Returns all specified trade allowance charges for the trade line item
+        /// </summary>
+        /// <returns></returns>
+        public IList<TradeAllowanceCharge> GetSpecifiedTradeAllowanceCharges()
+        {
+            return this.SpecifiedTradeAllowanceCharges;
+        } // !GetSpecifiedTradeAllowanceCharges()
 
 
         public void SetDeliveryNoteReferencedDocument(string deliveryNoteId, DateTime? deliveryNoteDate)
