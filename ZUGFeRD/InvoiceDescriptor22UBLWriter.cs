3586c52a802252e03d7a183bb7a9417f7112f2ec
Dai Duong Nguyen <ndadu@gmx.net>
2024-10-04 01:12:14 +0200

    fix wrong format UnitQuantity in TradeLineItem

--- ZUGFeRD/InvoiceDescriptor22UBLWriter.cs
+++ ZUGFeRD/InvoiceDescriptor22UBLWriter.cs
@@ -407,7 +407,7 @@ namespace s2industries.ZUGFeRD
 
                 Writer.WriteStartElement("cbc:BaseQuantity"); // BT-149
                 Writer.WriteAttributeString("unitCode", tradeLineItem.UnitCode.EnumToString()); // BT-150
-                Writer.WriteValue(tradeLineItem.UnitQuantity.ToString());
+                Writer.WriteValue(_formatDecimal(tradeLineItem.UnitQuantity));
                 Writer.WriteEndElement();
 
                 IList<TradeAllowanceCharge> charges = tradeLineItem.GetTradeAllowanceCharges();
