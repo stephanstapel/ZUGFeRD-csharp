f348822ef68b8e7baa0263b7bec72207c18fc681
Michael Herrlein <m.herrlein@schrempp-edv.de>
2024-11-13 17:02:29 +0100

    readded usings

--- ZUGFeRD/InvoiceDescriptor22UblReader.cs
+++ ZUGFeRD/InvoiceDescriptor22UblReader.cs
@@ -19,8 +19,11 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
 
 
 namespace s2industries.ZUGFeRD
