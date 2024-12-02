46a59e14265e1f6e88a03359d11ffe6bbcb390d3
Stephan <stephan.stapel@web.de>
2024-12-02 22:14:57 +0100

    test case correction

--- ZUGFeRD.PDF.Test/LoadTests.cs
+++ ZUGFeRD.PDF.Test/LoadTests.cs
@@ -10,7 +10,7 @@ namespace ZUGFeRD.PDF.Test
         public async Task BasicLoadExampleFile()
         {
             string path = @"..\..\..\..\documentation\zugferd23en\Examples\4. EXTENDED\EXTENDED_Warenrechnung\EXTENDED_Warenrechnung.pdf";
-            InvoiceDescriptor desc = await InvoiceDescriptorPdfExtension.LoadFromPdfAsync(path);
+            InvoiceDescriptor desc = await InvoicePdfProcessor.LoadFromPdfAsync(path);
 
             Assert.IsNotNull(desc);
             Assert.AreEqual("R87654321012345", desc.InvoiceNo);
@@ -21,7 +21,7 @@ namespace ZUGFeRD.PDF.Test
         public async Task BasicLoadNonExistingFile()
         {
             string path = @"doesnotexist.pdf";            
-            await Assert.ThrowsExceptionAsync<FileNotFoundException>(() => InvoiceDescriptorPdfExtension.LoadFromPdfAsync(path));
+            await Assert.ThrowsExceptionAsync<FileNotFoundException>(() => InvoicePdfProcessor.LoadFromPdfAsync(path));
         } // !BasicLoadNonExistingFile()
     }
 }
