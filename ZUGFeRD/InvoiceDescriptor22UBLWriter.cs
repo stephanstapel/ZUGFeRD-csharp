c8fb5891e9a59f7f371d624f916915ef3d77651d
ck-993 <kahlert.christian@gmx.de>
2025-01-17 11:11:32 +0100

    Changed order of Nodes

--- ZUGFeRD/InvoiceDescriptor22UBLWriter.cs
+++ ZUGFeRD/InvoiceDescriptor22UBLWriter.cs
@@ -430,19 +430,6 @@ namespace s2industries.ZUGFeRD
 
                 Writer.WriteElementString("cbc", "ChargeIndicator", tradeAllowanceCharge.ChargeIndicator ? "true" : "false");
 
-                Writer.WriteStartElement("cbc", "Amount"); // BT-92 / BT-99
-                Writer.WriteAttributeString("currencyID", this.Descriptor.Currency.EnumToString());
-                Writer.WriteValue(_formatDecimal(tradeAllowanceCharge.ActualAmount));
-                Writer.WriteEndElement();
-
-                if (tradeAllowanceCharge.BasisAmount != null)
-                {
-                    Writer.WriteStartElement("cbc", "BaseAmount"); // BT-93 / BT-100
-                    Writer.WriteAttributeString("currencyID", this.Descriptor.Currency.EnumToString());
-                    Writer.WriteValue(_formatDecimal(tradeAllowanceCharge.BasisAmount));
-                    Writer.WriteEndElement();
-                }
-
                 if (tradeAllowanceCharge.ReasonCode != AllowanceReasonCodes.Unknown)
                 {
                     Writer.WriteStartElement("cbc", "AllowanceChargeReasonCode"); // BT-97 / BT-104
@@ -454,6 +441,19 @@ namespace s2industries.ZUGFeRD
                 {
                     Writer.WriteStartElement("cbc", "AllowanceChargeReason"); // BT-97 / BT-104
                     Writer.WriteValue(tradeAllowanceCharge.Reason);
+                    Writer.WriteEndElement();
+                }
+
+                Writer.WriteStartElement("cbc", "Amount"); // BT-92 / BT-99
+                Writer.WriteAttributeString("currencyID", this.Descriptor.Currency.EnumToString());
+                Writer.WriteValue(_formatDecimal(tradeAllowanceCharge.ActualAmount));
+                Writer.WriteEndElement();
+
+                if (tradeAllowanceCharge.BasisAmount != null)
+                {
+                    Writer.WriteStartElement("cbc", "BaseAmount"); // BT-93 / BT-100
+                    Writer.WriteAttributeString("currencyID", this.Descriptor.Currency.EnumToString());
+                    Writer.WriteValue(_formatDecimal(tradeAllowanceCharge.BasisAmount));
                     Writer.WriteEndElement();
                 }
 
