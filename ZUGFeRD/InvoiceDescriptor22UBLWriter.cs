8b07aabe056f45154d06684ede4d4e09d0aac1f0
ck-993 <kahlert.christian@gmx.de>
2025-02-03 18:37:36 +0100

    Fixxed Merge problem

--- ZUGFeRD/InvoiceDescriptor22UBLWriter.cs
+++ ZUGFeRD/InvoiceDescriptor22UBLWriter.cs
@@ -469,13 +469,6 @@ namespace s2industries.ZUGFeRD
                     Writer.WriteEndElement();
                 }
 
-                if (!string.IsNullOrWhiteSpace(tradeAllowanceCharge.Reason))
-                {
-                    Writer.WriteStartElement("cbc", "AllowanceChargeReason"); // BT-97 / BT-104
-                    Writer.WriteValue(tradeAllowanceCharge.Reason);
-                    Writer.WriteEndElement();
-                }
-
                 Writer.WriteStartElement("cbc", "Amount"); // BT-92 / BT-99
                 Writer.WriteAttributeString("currencyID", this.Descriptor.Currency.EnumToString());
                 Writer.WriteValue(_formatDecimal(tradeAllowanceCharge.ActualAmount));
