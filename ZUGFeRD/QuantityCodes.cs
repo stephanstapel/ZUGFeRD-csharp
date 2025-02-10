3f3f280b4a9b44c8edac8a5f2ad23d8fc5a5352f
PeterSawatzki <peter.sawatzki@mggm-software.de>
2025-02-10 20:08:13 +0100

    add QuantityCode KTM

--- ZUGFeRD/QuantityCodes.cs
+++ ZUGFeRD/QuantityCodes.cs
@@ -169,6 +169,12 @@ namespace s2industries.ZUGFeRD
         KMT,
 
         /// <summary>
+        /// Kilometer
+        /// Abkürzung: km (Rec20r16)
+        /// </summary>
+        KTM,
+
+        /// <summary>
         /// Kilowattstunde
         /// Abkürzung: kWh
         /// </summary>
