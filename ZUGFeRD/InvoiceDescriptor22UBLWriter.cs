356655395d7f1d0888ffd2563e92e8f0cd57c027
Stephan <stephan.stapel@web.de>
2025-12-13 21:41:20 +0100

    corrected ubl type detection

--- ZUGFeRD/InvoiceDescriptor22UBLWriter.cs
+++ ZUGFeRD/InvoiceDescriptor22UBLWriter.cs
@@ -43,7 +43,7 @@ namespace s2industries.ZUGFeRD
 
             this._Descriptor = descriptor;
             this._Writer = new ProfileAwareXmlTextWriter(stream, descriptor.Profile, options?.AutomaticallyCleanInvalidCharacters ?? false);
-            bool isInvoice = _IsInvoiceAccordingToEn16931(this._Descriptor.Type);
+            bool isInvoice = _IsInvoiceAccordingToUBLSpecification(this._Descriptor.Type);
 
             Dictionary<string, string> namespaces = new Dictionary<string, string>()
             {
@@ -1140,27 +1140,47 @@ namespace s2industries.ZUGFeRD
         }
 
 
-        private bool _IsInvoiceAccordingToEn16931(InvoiceType type)
+        private bool _IsInvoiceAccordingToUBLSpecification(InvoiceType type)
         {
             switch (type)
             {
-                case InvoiceType.CreditNoteRelatedToGoodsOrServices:
-                case InvoiceType.CreditNoteRelatedToFinancialAdjustments:
-                case InvoiceType.SelfBilledCreditNote:
-                case InvoiceType.ConsolidatedCreditNoteGoodsAndServices:
-                case InvoiceType.CreditNoteForPriceVariation:
-                case InvoiceType.DelcredereCreditNote:
-                case InvoiceType.CreditNote:
-                case InvoiceType.FactoredCreditNote:
-                case InvoiceType.OcrPaymentCreditNote:
-                case InvoiceType.ReversalOfCredit:
-                case InvoiceType.SelfBilledFactoredCreditNote:
-                case InvoiceType.PrepaymentCreditNoteCorrected:
-                case InvoiceType.ForwardersCreditNote:
+                case InvoiceType.RequestForPayment: // 71
+                case InvoiceType.DebitNoteRelatedToGoodsOrServices: // 80
+                case InvoiceType.MeteredServicesInvoice: // 82
+                case InvoiceType.DebitnoteRelatedToFinancialAdjustments: // 84
+                case InvoiceType.TaxNotification: // 102
+                case InvoiceType.FinalPaymentRequestBasedOnCompletionOfWork: // 218
+                case InvoiceType.PaymentRequestForCompletedUnits: // 219
+                case InvoiceType.PartialInvoice: // 326
+                case InvoiceType.CommercialInvoiceWithPackingList: // 331
+                case InvoiceType.Invoice: // 380
+                case InvoiceType.CommissionNote: // 382
+                case InvoiceType.DebitNote: // 383
+                case InvoiceType.Correction: // 384
+                case InvoiceType.PrepaymentInvoice: // 386
+                case InvoiceType.TaxInvoice: // 388
+                case InvoiceType.SelfBilledInvoice: // 389
+                case InvoiceType.FactoredInvoice: // 393
+                case InvoiceType.ConsignmentInvoice: // 395
+                case InvoiceType.ForwardersInvoiceDiscrepancyReport: // 553
+                case InvoiceType.InsurersInvoice: // 575
+                case InvoiceType.ForwardersInvoice: // 623
+                case InvoiceType.FreightInvoice: // 780
+                case InvoiceType.ClaimNotification: // 817
+                case InvoiceType.ConsularInvoice: // 870
+                case InvoiceType.PartialConstructionInvoice: // 875
+                case InvoiceType.PartialFinalConstructionInvoice: // 876
+                case InvoiceType.FinalConstructionInvoice: // 877
+                    return true;
+                case InvoiceType.CreditNoteRelatedToGoodsOrServices: // 81
+                case InvoiceType.CreditNoteRelatedToFinancialAdjustments: // 83
+                case InvoiceType.CreditNote: // 381
+                case InvoiceType.FactoredCreditNote: // 396
+                case InvoiceType.ForwardersCreditNote: // 532
                     return false;
                 default:
-                    return true;
+                    throw new NotImplementedException($"Invoice type {type} not implemented in UBL writer.");
             }
-        } // !_IsInvoiceAccordingToEn16931()
+        } // !_IsInvoiceAccordingToUBLSpecification()
     }
 }
